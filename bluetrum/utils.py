453766606280419cf94e1ebe72a81c95dd2af0a2
Andrey Grigoryev <andrey-grigorev-2006@yandex.ru>
2024-07-06 01:50:43 +0300

    docstrings for align_by/align_to

--- bluetrum/utils.py
+++ bluetrum/utils.py
@@ -1,10 +1,10 @@
+
 __all__ = [
     'hexdump',
     'align_by',
     'align_to',
     'anyint',
 ]
-
 
 try:
     from hexdump import hexdump
@@ -14,15 +14,15 @@ except ImportError:
         for off in range(0, len(data), 16):
             print(f'{off:08X}: {data[off:off+16].hex(" ")}')
 
-
 def align_by(value, alignment):
+    """ Get a value to pad/align the value to a specified alignment """
     n = value % alignment
     if n > 0: n = alignment - n
     return n
 
 def align_to(value, alignment):
+    """ Align/pad a value to a specified alignment """
     return value + align_by(value, alignment)
-
 
 def anyint(s):
     return int(s, 0)
