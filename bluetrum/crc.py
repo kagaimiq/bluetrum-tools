c135af99684cdec9030d6cd6f9e71a9e991c0492
Andrey Grigoryev <andrey-grigorev-2006@yandex.ru>
2024-07-06 01:49:44 +0300

    the CRC32 is reversed
    At least that's the case for a device CRC calculation
    command of the bootloader.

--- bluetrum/crc.py
+++ bluetrum/crc.py
@@ -4,4 +4,4 @@ __all__ = ['ab_crc16', 'ab_crc32']
 import crcmod
 
 ab_crc16 = crcmod.mkCrcFun(0x11021, xorOut=0, rev=False)
-ab_crc32 = crcmod.mkCrcFun(0x104C11DB7, xorOut=0, rev=False)
+ab_crc32 = crcmod.mkCrcFun(0x104C11DB7, xorOut=0, rev=True)
